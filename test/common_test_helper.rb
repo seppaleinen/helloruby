c38be82365518917fb7921d2b0d8c79c16093bbc
david eriksson <davideriksson@swedenmail.com>
2015-06-14 16:51:58 +0200

    coverage filter for testfolder

--- test/common_test_helper.rb
+++ test/common_test_helper.rb
@@ -3,5 +3,6 @@ Coveralls.wear_merged!
 require 'simplecov'
 require 'codeclimate-test-reporter'
 SimpleCov.add_filter 'vendor'
+SimpleCov.add_filter 'test'
 SimpleCov.formatters = []
 SimpleCov.start
