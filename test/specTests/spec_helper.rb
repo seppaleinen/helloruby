3d3e16258131e631a9954c6a18baf82a2e2f354b
david eriksson <davideriksson@swedenmail.com>
2015-07-27 22:22:17 +0200

    Test with coverage

--- test/specTests/spec_helper.rb
+++ test/specTests/spec_helper.rb
@@ -1,2 +1,11 @@
-require_relative '../common_test_helper'
+#require_relative '../common_test_helper'
+require 'coveralls'
+Coveralls.wear_merged!
+require 'simplecov'
+require 'codeclimate-test-reporter'
+SimpleCov.add_filter 'vendor'
+SimpleCov.add_filter 'test'
+#SimpleCov.formatters = []
+SimpleCov.command_name 'Spec tests'
+SimpleCov.start
 require 'rspec'
